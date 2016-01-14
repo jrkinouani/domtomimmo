2a20ed4710a34e912d0466306f1c34e374c4a172
Mehdi FARSI <mehdifarsi.pro@gmail.com>
2016-01-14 17:26:07 +0100

    Enable iframe in Heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ module Domtomimmo
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # http://www.seanbehan.com/how-to-enable-iframe-support-on-heroku-with-ruby-on-rails-and-sinatra
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
